789c603a402f8af8182b98c19076effffd4f6b3d
scheslen <antgalin@mail.ru>
2025-05-23 18:17:58 +0300

    refactor: delete unused code

--- flower-shop/src/components/Profile/Profile.tsx
+++ flower-shop/src/components/Profile/Profile.tsx
@@ -1,22 +1,8 @@
 import "./profile.css";
 import { CreateSection } from "./profileCreate.tsx";
-// import { registerSubmit } from "./registerSubmit";
-//import { Link } from "react-router-dom";
 import { aProfileSections } from "./profileData.ts";
-//import { useNavigate } from "react-router-dom";
-// //import { isValidForm } from "./registerValid.ts";
-// import { registerSubmitButton } from "./registerSubmit";
 
 export const Main = () => {
-  // const greenColor = "#4a4e37";
-  // const navigate = useNavigate();
-  // //  const rSubmit = async (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
-  // const rSubmit = async () => {
-  //   if (await registerSubmitButton()) {
-  //     //  if (isValidForm(false)) {
-  //     navigate("/");
-  //   }
-  // };
   return (
     <div>
       <div
@@ -24,35 +10,14 @@ export const Main = () => {
         style={{ backgroundImage: "url('img/bgroses33.png')" }}
       >
         <h2> User Profile Information</h2>
-        {/* <form onSubmit={(e) => registerSubmit(e)}> */}
         <div className="profile__div">
           {aProfileSections.map((iSection) => (
             <CreateSection section={iSection} key={iSection.id} />
           ))}
-          {/* <input type='submit'>Create account</input> */}
-          {/* <button className="register_btn" onClick={(e) =>registerSubmitButton(e)}> */}
 
-          {/* <div className="register__submit-txt">The form is filled correctly</div> */}
-          {/* <div className="register__submit-txt"> */}
-          {/* Not all form fields are filled in correctly */}
-          {/* </div>
-          <button
-            className="register__submit-btn inactive"
-            onClick={rSubmit} //(e) => registerSubmitButton(e)
-          > */}
           <button className="profile__submit-btn">Edit</button>
 
-          {/* <p className="register__switch-login">
-            {/* Already have an account?{" "} */}
-          {/* <span className="register__green">
-              <Link to="/" style={{ color: greenColor }}>
-                Home
-              </Link>
-            </span>
-            !
-          </p> */}
         </div>
-        {/* </form>*/}
       </div>
     </div>
   );
--- flower-shop/src/components/Profile/profileCreate.tsx
+++ flower-shop/src/components/Profile/profileCreate.tsx
@@ -18,8 +18,6 @@ export function CreateSection({ section }: IPropsSection) {
     <div className="profile__section">
       <h3>{section.title}</h3>
       <ProfInfo aFields={section.aFields} />
-      {/* <RegInputs aFields={section.aFields} />
-      <RegChecks aChecks={section.aChecks} /> */}
     </div>
   );
 }
@@ -30,23 +28,12 @@ function ProfInfo({ aFields }: IPropsInput) {
       {aFields.map((iField, i) => (
         <div className="profile__info" key={i + 3}>
           <h4>{iField.label}: </h4>
-          {/* {iField.label === "Country" ? (
-            <RegSelect />
-          ) : ( */}
           <p
             className="profile__info_txt"
-            //type={iField.type}
-            //name={iField.name}
-            // placeholder={iField.label + ": " + iField.placeholder}
-            //placeholder={iField.placeholder}
-            // onBlur={(e) => blurHandler(e)}
-            //onChange={(e) => inputHandler(e)}
             key={iField.id}
-            //defaultValue={iField.type === "date" ? "2000-01-01" : ""} //min="2023-01-01" max="2023-12-31"
           >
             {iField.placeholder}
           </p>
-          {/* <div className="register__input-error"></div> */}
         </div>
       ))}
     </div>
