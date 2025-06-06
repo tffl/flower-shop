fae268555d8b596a404966fc294f566315b4048b
scheslen <antgalin@mail.ru>
2025-06-06 20:33:50 +0300

    feat: implement empty cart

--- flower-shop/src/components/Basket/Basket.tsx
+++ flower-shop/src/components/Basket/Basket.tsx
@@ -1,13 +1,46 @@
 import "./basket.css";
+import { Link } from "react-router-dom";
+
 export const Main = () => {
+  let goodsQuantityAll = 0;
+  let goodsCostAll = 287.53;
   return (
     <div className="basket__main">
-      <h2> Basket </h2>
-      <div className="dummy" style={{ backgroundImage: "url('img/back.png')" }}>
-        <p>Page is not available yet.</p>
-        <p>
-          We are doing our best to get it up and running as soon as possible
-        </p>
+      <div
+        className="basket__content"
+        style={{ backgroundImage: "url('img/bgroses23.png')" }}
+      >
+        <h2> Basket </h2>
+        <div className="basket__total">
+          {goodsQuantityAll < 1 ? (
+            <>
+              <p>Your cart is empty yet</p>
+               You can begin shopping:{" "}
+            <Link to="/catalog" className="register__link">
+              Catalog
+            </Link>
+            </>
+          ) : (
+            <>
+              <p>
+                {" "}
+                You have selected{" "}
+                <span className="basket__txt-mark">
+                  {goodsQuantityAll}
+                </span>{" "}
+                items. Total cost:{" "}
+                <span className="basket__txt-mark">${goodsCostAll}</span>
+              </p>
+               <p>
+            {" "}
+            You can continue shopping:{" "}
+            <Link to="/catalog" className="register__link">
+              Catalog
+            </Link>
+          </p>
+            </>
+          )}
+        </div>
       </div>
     </div>
   );
